784907879c6a95dd1f2c4bbd7dd08602611e8d09
Déborah MOREL <debbymorel@yahoo.com>
2023-03-26 21:14:10 +0200

    Sudoku

--- Grille.py
+++ Grille.py
@@ -1,69 +1,6 @@
 import tkinter as tk
 import random as rd
 
-G1 = [[9, 1, 4, 5, 2, 3, 8, 7, 6] ,  
-      [3, 7, 5, 9, 6, 8, 4, 2, 1] , 
-      [6, 2, 8, 7, 1, 4, 3, 5, 9] , 
-      [2, 9, 6, 4, 5, 1, 7, 8, 3] , 
-      [5, 8, 3, 6, 7, 2, 9, 1, 4] , 
-      [1, 4, 7, 8, 3, 9, 5, 6, 2] , 
-      [4, 6, 9, 1, 8, 7, 2, 3, 5] , 
-      [7, 3, 1, 2, 4, 5, 6, 9, 8] , 
-      [8, 5, 2, 3, 9, 6, 1, 4, 7]]
-
-
-
-#Cette fonction sert à mélanger les lignes dans une même rangée de 3 
-
-def mélangerlignes(G):
-    L = []
-    for i in range(0, 3):
-        a = rd.randint(0, 2)
-        L.append(G[a])
-        del G[a]
-        a = rd.randint(0, 1)
-        L.append(G[a])
-        del G[a]
-        L.append(G[0])
-        del G[0]
-    return L
-
-#Cette fonction sert à mélanger les colonnes dans une même rangée de 3
-
-def mélangercolonnes(G):
-    L = [[], [], [], [], [], [], [], [], []]
-    for j in range(0, 3):
-        n = rd.randint(0, 2)
-        for i in range(0, 9):
-            L[i].append(G[i][n])
-            del G[i][n]
-        n = rd.randint(0, 1)
-        for i in range(0, 9):
-            L[i].append(G[i][n])
-            del G[i][n]
-        for i in range(0, 9):
-            L[i].append(G[i][0])
-            del G[i][0]
-    return L
-
-    
-#Cette fonction sert à crypter une grille grâce à une clé de cryptage prise au hasard 
-
-def cryptage(G):
-    L = []
-    while len(L) < 9 :
-        a = rd.randint(1, 9)
-        if a not in L :
-            L.append(a)
-    for i in range(0, 9):
-        for j in range(0, 9):
-            for n in range(0, 9):
-                if G[i][j] == n + 1 :
-                    G[i][j] = L[n]
-                    break
-            
-    return G
-
 # Création de la fenêtre
 
 Taille = 600
@@ -87,8 +24,8 @@ for i in range(Case+1):
 
 # Bouton pour commencer la partie
 
-def affichage(G):
-    a = mélangerlignes(G)
+def affichage(Grille):
+    a = mélangerlignes(Grille)
     b = mélangercolonnes(a)
     c = cryptage(b)
 
